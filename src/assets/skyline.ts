d6ee49fe8718d4d07270841b2d29ee77fa95762d
Rick Osborne <ricko@rickosborne.org>
2020-08-20 23:41:47 -0700

    Use details for spoilers ; reimplement list styles in reset (#92)

--- src/assets/skyline.ts
+++ src/assets/skyline.ts
@@ -6,24 +6,32 @@ document.addEventListener('DOMContentLoaded', function onDomContentLoaded() {
 			return;
 		}
 		const isBlock = spoilerEl.classList.contains('block') || ['blockquote', 'div'].includes(spoilerEl.tagName.toLowerCase());
-		const warningType = isBlock ? 'DIV' : 'SPAN';
-		const checkboxEl = document.createElement('INPUT') as HTMLInputElement;
-		const labelEl = document.createElement('LABEL') as HTMLLabelElement;
+		const warningType = isBlock ? 'DETAILS' : 'SPAN';
 		const warningEl = document.createElement(warningType) as HTMLSpanElement;
-		checkboxEl.type = 'checkbox';
-		checkboxEl.classList.add('spoiler-checkbox');
-		labelEl.classList.add('spoiler-label');
-		warningEl.classList.add('spoiler-warning');
 		if (isBlock) {
+			const summaryEl = document.createElement('SUMMARY') as HTMLElement;
+			summaryEl.appendChild(document.createTextNode('Spoiler'));
+			warningEl.appendChild(summaryEl);
 			warningEl.classList.add('block');
+			parentElement.insertBefore(warningEl, spoilerEl);
+			parentElement.removeChild(spoilerEl);
+			warningEl.appendChild(spoilerEl);
+			summaryEl.classList.add('spoiler-warning');
+		} else {
+			const labelEl = document.createElement('LABEL') as HTMLLabelElement;
+			const checkboxEl = document.createElement('INPUT') as HTMLInputElement;
+			checkboxEl.type = 'checkbox';
+			checkboxEl.classList.add('spoiler-checkbox');
+			labelEl.appendChild(checkboxEl);
+			labelEl.classList.add('spoiler-label');
 			labelEl.classList.add('block');
+			warningEl.append(document.createTextNode(' (spoiler) '));
+			parentElement.insertBefore(labelEl, spoilerEl);
+			parentElement.removeChild(spoilerEl);
+			labelEl.appendChild(warningEl);
+			labelEl.appendChild(spoilerEl);
+			warningEl.classList.add('spoiler-warning');
 		}
-		warningEl.append(document.createTextNode(' (spoiler) '));
-		parentElement.insertBefore(labelEl, spoilerEl);
-		parentElement.removeChild(spoilerEl);
-		labelEl.appendChild(checkboxEl);
-		labelEl.appendChild(warningEl);
-		labelEl.appendChild(spoilerEl);
 	});
 
 	document.querySelectorAll('.fullscreen-able').forEach(fullscreenAble => {
